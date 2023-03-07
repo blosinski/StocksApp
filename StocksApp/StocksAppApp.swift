403041b1c5c7cc59754fa66b5b279f1359ab84fa
blosinski <brendan.losinski@yahoo.com>
2023-03-07 10:58:31 -0600

    Initial Commit

---
+++ StocksApp/StocksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksAppApp.swift
+//  StocksApp
+//
+//  Created by Brendan Losinski on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
